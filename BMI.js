4f7712e6f31ed58196e5200a3c96c0bf5402e20e
Aftertaste26 <20hardrock12@gmail.com>
2020-10-29 22:59:33 +0800

    refactor: eliminated redundant code

--- BMI.js
+++ BMI.js
@@ -1,15 +1,6 @@
 const metricBMI = (weight, height) => {
   const bmi = parseFloat((weight / Math.pow(height / 100, 2)).toFixed(1));
-
-  const status =
-    bmi < 18.5
-      ? "Underweight"
-      : bmi >= 18.5 && bmi <= 24.9
-      ? "Normal weight"
-      : bmi >= 25 && bmi <= 29.9
-      ? "Overweight"
-      : "Obesity";
-
+  const status = weightStatus(bmi);
   return { weight, height, bmi, status };
 };
 
@@ -18,17 +9,17 @@ const englishBMI = (weight, { feet, inches }) => {
   const bmi = parseFloat(
     ((703 * weight) / Math.pow(feet * 12 + inches, 2)).toFixed(1)
   );
-
-  const status =
-    bmi < 18.5
-      ? "Underweight"
-      : bmi >= 18.5 && bmi <= 24.9
-      ? "Normal weight"
-      : bmi >= 25 && bmi <= 29.9
-      ? "Overweight"
-      : "Obesity";
-
+  const status = weightStatus(bmi);
   return { weight, height, bmi, status };
 };
+
+const weightStatus = (bmi) =>
+  bmi < 18.5
+    ? "Underweight"
+    : bmi >= 18.5 && bmi <= 24.9
+    ? "Normal weight"
+    : bmi >= 25 && bmi <= 29.9
+    ? "Overweight"
+    : "Obesity";
 
 module.exports = { metricBMI, englishBMI };
