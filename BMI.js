b1a50210cf75854a2969f43e064394cf685966a1
Aftertaste26 <20hardrock12@gmail.com>
2020-10-29 20:12:40 +0800

    refactor: reduced code repeatition

--- BMI.js
+++ BMI.js
@@ -1,12 +1,16 @@
 const metricBMI = (weight, height) => {
   const bmi = parseFloat((weight / Math.pow(height / 100, 2)).toFixed(1));
 
-  if (bmi < 18.5) return { weight, height, bmi, status: "Underweight" };
-  if (bmi >= 18.5 && bmi <= 24.9)
-    return { weight, height, bmi, status: "Normal weight" };
-  if (bmi >= 25 && bmi <= 29.9)
-    return { weight, height, bmi, status: "Overweight" };
-  return { weight, height, bmi, status: "Obesity" };
+  const status =
+    bmi < 18.5
+      ? "Underweight"
+      : bmi >= 18.5 && bmi <= 24.9
+      ? "Normal weight"
+      : bmi >= 25 && bmi <= 29.9
+      ? "Overweight"
+      : "Obesity";
+
+  return { weight, height, bmi, status };
 };
 
 console.log(metricBMI(60, 167));
