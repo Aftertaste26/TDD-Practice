3bceb12adb3babc6342859a54617965919dd6028
Aftertaste26 <20hardrock12@gmail.com>
2020-10-30 14:27:50 +0800

    feat: virtual swap exchange paper

--- src/exchangePapers.js
+++ src/exchangePapers.js
@@ -1,10 +1,16 @@
 const swap = (classRoll) => {
+  let output = [];
   for (let index = 0; index < classRoll.length; index += 2) {
-    classRoll[index] = classRoll[index + 1];
-    classRoll[index + 1] = classRoll[index];
+    if (classRoll.length % 2 != 0 && index == classRoll.length - 1) {
+      index--;
+      output[index] = classRoll[index + 1];
+      output[index + 1] = classRoll[index - 1];
+    } else {
+      output[index] = classRoll[index + 1];
+      output[index + 1] = classRoll[index];
+    }
   }
-
-  return classRoll;
+  return output;
 };
 
 module.exports = { swap };
--- test/exchangePapers.test.js
+++ test/exchangePapers.test.js
@@ -4,4 +4,8 @@ describe("virtual exchange paper", () => {
   it("swap paper with the person adjacent to other person in the classroll with even number of students", () => {
     expect(swap(["A", "B", "C", "D"])).toStrictEqual(["B", "A", "D", "C"]);
   });
+
+  it("swap paper with the person adjacent to other person in the classroll with odd number of students", () => {
+    expect(swap(["X", "Y", "Z"])).toStrictEqual(["Y", "Z", "X"]);
+  });
 });
