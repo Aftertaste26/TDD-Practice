2afab6146769fa092c68c92f63c1a92ba0b271e1
Aftertaste26 <20hardrock12@gmail.com>
2020-10-30 18:25:44 +0800

    refactor: reduced redundant code

--- src/exchangePapers.js
+++ src/exchangePapers.js
@@ -1,10 +1,9 @@
 const swap = (classRoll) => {
   let output = [];
   for (let index = 0; index < classRoll.length; index += 2) {
-    if (classRoll.length % 2 != 0 && index == classRoll.length - 1) {
-      index--;
-      output[index] = classRoll[index + 1];
-      output[index + 1] = classRoll[index - 1];
+    if (classRoll.length % 2 !== 0 && index === classRoll.length-1) {
+      output[index-1] = classRoll[index];
+      output[index] = classRoll[index - 2];
     } else {
       output[index] = classRoll[index + 1];
       output[index + 1] = classRoll[index];
@@ -15,12 +14,15 @@ const swap = (classRoll) => {
 
 const counterClockWise = (n, classroll) => {
   let output = [];
-  for (let index = 0; index < classroll.length; index++) {
-    output[index] =
-      classroll[(classroll.length - n + index) % classroll.length];
+  for (const index in classroll) {
+    output = [
+      ...output,
+      classroll[(classroll.length - n + Number(index)) % classroll.length],
+    ];
   }
 
   return output;
 };
+
 
 module.exports = { swap, counterClockWise };
